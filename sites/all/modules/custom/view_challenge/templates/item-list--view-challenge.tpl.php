d5dbfb261fec6d1c366c8c8d0be16c9d9b10d5fe
Vincent Jones <vjones@dhcp-16-4-231.bravo>
2015-07-31 17:20:57 -0500

    things work

---
+++ sites/all/modules/custom/view_challenge/templates/item-list--view-challenge.tpl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The raw implementation of the item-list--view-challenge template
+ *
+ * @var array $items
+ *   The Node objects that were retrived in >>
+ *
+ * @var array $form_block
+ *   The prepared form render array
+ */
+$break = 'point';
+
+?>
+
+<div>
+  <?php foreach($items as $node): ?>
+  <div class="node">
+    <?php print $node; ?>
+  </div>
+  <?php endforeach; ?>
+</div>
+<div>
+  <?php print render($form_block); ?>
+</div>
